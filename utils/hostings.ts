d223954d3fa42831f7d7837c571ddf93fe6fbb58
System Administrator <sciemesfin55@gmail.com>
2022-11-16 12:24:13 +0300

    updates

---
+++ utils/hostings.ts
@@ -0,0 +1,67 @@
+const hostings: any = [
+    {
+        title: "Basic", amount: '50',
+        features: [
+            "For Fast Static sites",
+            "HTML,CSS & JS",
+            "Cloudflare’s edge network",
+            "UNLIMITED Visitors",
+            "FTP Access",
+            ".COM Domain Included",
+            "SSL Certificates Included"
+        ]
+    },
+    {
+        title: "Standard",
+        amount: '60',
+        features: [
+            "For Fast Static sites",
+            "HTML,CSS & JS",
+            "Cloudflare’s edge network",
+            "UNLIMITED Visitors",
+            "FTP Access",
+            ".COM Domain Included",
+            "SSL Certificates Included",
+            "Optimized for Wordpress",
+            "Website Acceleration Suite",
+            "Advanced Wordpress Security",
+            "Managed updates & patches",
+            "Daily Virus Scanning"
+        ]
+    },
+    {
+        title: "Business",
+        amount: '65',
+        features: [
+            "For Fast Static sites",
+            "HTML,CSS & JS",
+            "Cloudflare’s edge network",
+            "UNLIMITED Visitors",
+            "FTP Access",
+            ".COM Domain Included",
+            "SSL Certificates Included",
+            "Optimized for Wordpress",
+            "Website Acceleration Suite",
+            "Managed updates & patches",
+        ]
+    },
+    {
+        title: "Managed Hosting",
+        amount: '70',
+        features: [
+            "For Fast Static sites",
+            "HTML,CSS & JS",
+            "Cloudflare’s edge network",
+            "UNLIMITED Visitors",
+            "FTP Access",
+            ".COM Domain Included",
+            "SSL Certificates Included",
+            "Optimized for Wordpress",
+            "Website Acceleration Suite",
+            "Advanced Wordpress Security",
+            "Managed updates & patches",
+            "Daily Virus Scanning"
+        ]
+    }
+]
+export { hostings }
