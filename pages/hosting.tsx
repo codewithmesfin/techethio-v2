e065d01481c2466f23e72f479241bb31f7a26047
System Administrator <sciemesfin55@gmail.com>
2022-11-16 12:28:10 +0300

    updates

--- pages/hosting.tsx
+++ pages/hosting.tsx
@@ -26,7 +26,7 @@ export default function Hosting() {
                             <div className="p-10">
                                 <ul>
                                     {
-                                        x.features.map((y, j) => <li key={j} className="flex items-center py-2">
+                                        x.features.map((y:any, j:number) => <li key={j} className="flex items-center py-2">
                                             <svg className="w-5 h-5 text-green-600 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
                                             </svg>
