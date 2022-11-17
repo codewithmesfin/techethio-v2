c9f8c256f68415d07d64d9bb1fd555461163b5e3
System Administrator <sciemesfin55@gmail.com>
2022-11-17 12:44:47 +0300

    save changes

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function GoogleMap() {
-    return <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.403285710987!2d38.74504191493469!3d9.02692599150659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b8508bb02a269%3A0x989e18c2bc109aab!2zVGVjaCBFdGhpb3BpYSAvIOGJtOGKrSDhiqLhibXhi67hjLXhi6s!5e0!3m2!1sam!2set!4v1668672299918!5m2!1sam!2set"
+    return <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.7844583169103!2d38.78507091493462!3d8.991971192043325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85eb2e952955%3A0x2a0ca568eceaa264!2sTechEthio%20IT%20Solution%20PLC!5e0!3m2!1sam!2set!4v1668678246132!5m2!1sam!2set"
     width="100%"
     height="500"
     allowFullScreen={true}
